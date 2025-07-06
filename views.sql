b6937fd0ba3a8a8e3f884cdaa50a89a92a83baba
Cleave <cleavestone94@gmail.com>
2025-07-06 15:58:02 +0300

    Stored Procedures

--- views.sql
+++ views.sql
@@ -18,3 +18,28 @@ GROUP BY c.customer_id, c.customer_name, c.email;
 
 SELECT * FROM customer_summary2 WHERE total_amount_spent>10000;
 
+/*
+Create a stored procedure called get_customer_summary that takes one input parameter: 
+a numeric amount. The procedure should return a list of customers (customer_id, customer_name, 
+and email) along with their total_spent (calculated as the sum of quantity × price across all orders). 
+Only include customers whose total spending is less than the given amount.
+*/
+
+DELIMITER //
+
+CREATE PROCEDURE get_customer_summary(IN amount DECIMAL)
+BEGIN
+     SELECT 
+         c.customer_id,
+         c.customer_name,
+         c.email,
+         SUM(o.quantity * p.price) AS total_spent
+     FROM customers AS c 
+     JOIN orders AS o USING(customer_id)
+     JOIN products AS p USING(product_id)
+     GROUP BY c.customer_id, c.customer_name, c.email
+     HAVING SUM(o.quantity * p.price) < amount;
+END //
+
+DELIMITER ;
+
